da966e711cf313aa85f25e5a088c962fa7995ca1
freitzzz <1160907@isep.ipp.pt>
2023-07-16 15:38:22 +0100

    feat: add example code

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,20 @@
-import sum from 'vault';
+import { Vault } from 'vault';
 
 function main() {
-	console.log(sum(40, 2));
+	// Create a vault
+	const vault = new Vault();
+
+	// Create some tokens
+	const token = 'my cool token';
+
+	// Store dependencies!
+	vault.store('my secret message', token);
+
+	// Look them up at some point in your program
+	const dependency = vault.lookup<string>(token);
+
+	// It should print "just lookup this dependency: my secret message"
+	console.info(`just lookup this dependency: ${dependency}`);
 }
 
 main();
